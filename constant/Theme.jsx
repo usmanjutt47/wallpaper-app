a273d4ccc2a69c61aa9b822aaf4de991c6cdd7b9
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-06-29 15:29:30 +0500

    theme.jsx

---
+++ constant/Theme.jsx
@@ -0,0 +1,20 @@
+export const Theme = {
+  colors: {
+    white: "#fff",
+    black: "#000",
+    grayGb: "#e5e5e5",
+    neutral: (opacity) => `rgba(10,10,10,${opacity})`,
+  },
+  fontWeight: {
+    medium: "500",
+    semibold: "600",
+    bold: "700",
+  },
+  radius: {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
+  },
+};
