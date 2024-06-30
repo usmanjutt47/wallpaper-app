2da0533327a4706800129e7c4545d7af02898983
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-06-30 08:53:50 +0500

    Update HomeScreen.jsx

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,14 +1,123 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useRef, useState } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Feather, FontAwesome6, Ionicons } from "@expo/vector-icons";
+import { theme } from "../../constant/Theme";
+import { hp, wp } from "../../helpers/Common";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
+  const paddingTop = top > 0 ? top + 10 : 30;
+  const [search, setSearch] = useState("");
+  const searchInputRef = useRef(null);
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  const handleChangeCategory = (cat) => {
+    setActiveCategory(cat);
+  };
+  console.log("Active", activeCategory);
   return (
-    <View>
-      <Text>HomeScree</Text>
+    <View style={[styles.container, { paddingTop }]}>
+      <View style={styles.header}>
+        <Pressable>
+          <Text style={styles.title}>Pixels</Text>
+        </Pressable>
+        <Pressable>
+          <FontAwesome6
+            name="bars-staggered"
+            size={22}
+            color={theme.colors.neutral(0.7)}
+          />
+        </Pressable>
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ gap: 15 }}>
+        <View style={styles.searchBar}>
+          <View style={styles.searchIcon}>
+            <Feather
+              name="search"
+              size={24}
+              color={theme.colors.neutral(0.4)}
+            />
+          </View>
+          <TextInput
+            placeholder="Search for photos..."
+            style={styles.searchInput}
+            ref={searchInputRef}
+            value={search}
+            onChangeText={(value) => setSearch(value)}
+          />
+          {search && (
+            <Pressable style={styles.closeIcon}>
+              <Ionicons
+                name="close"
+                size={24}
+                color={theme.colors.neutral(0.6)}
+              />
+            </Pressable>
+          )}
+        </View>
+        <View style={search.categories}>
+          <Categories
+            activeCategory={activeCategory}
+            handleChangeCategory={handleChangeCategory}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 15,
+  },
+  header: {
+    marginHorizontal: wp(8),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: hp(8),
+    fontWeight: theme.fontWeight.semibold,
+    color: theme.colors.neutral(0.9),
+  },
+  searchBar: {
+    marginHorizontal: wp(8),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: theme.colors.grayGb,
+    backgroundColor: theme.colors.white,
+    padding: 6,
+    paddingLeft: 10,
+    borderRadius: theme.radius.lg,
+  },
+  searchIcon: {
+    padding: 8,
+  },
+  searchInput: {
+    flex: 1,
+    borderRadius: theme.radius.sm,
+    paddingVertical: 10,
+  },
+  closeIcon: {
+    padding: 8,
+    backgroundColor: theme.colors.neutral(0.1),
+    borderRadius: theme.radius.sm,
+  },
+});
